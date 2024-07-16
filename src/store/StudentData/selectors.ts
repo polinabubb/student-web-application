25d2787d2ef1a93c6606a3bee7bcb1d023936eff
polinabubb <polinabubb@gmail.com>
2024-07-16 15:32:34 +0500

    билд

--- src/store/StudentData/selectors.ts
+++
@@ -1,9 +0,0 @@
-import { NameSpace } from "../../const";
-import { State, Student } from "../../types";
-
-export const getStudents = (state: Pick<State, NameSpace.Data>): Student[] =>
-  state[NameSpace.Data].displayedStudents;
-
-export const getStudentDataLoadingStatus = (
-  state: Pick<State, NameSpace.Data>
-): boolean => state[NameSpace.Data].isStudentsLoading;
